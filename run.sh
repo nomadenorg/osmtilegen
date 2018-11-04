d1b7cf0aeb84ef6a2810373d8fc83302c3b04709
Mark Meyer <mark@ofosos.org>
2018-11-04 16:28:08 +0100

    Add info output to script

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 set -xe
 
+echo "Requesting spot instance..."
+
 HOSTNAME=$(python3 request.py)
 
 while ! nc -z $HOSTNAME 22; do   
@@ -9,7 +11,17 @@ while ! nc -z $HOSTNAME 22; do
 done
 
 scp -oStrictHostKeyChecking=no gen-tiles.py 'ubuntu@'$HOSTNAME:/tmp/gen-tiles.py
+
+echo "Generating maps..."
+
 ssh -oStrictHostKeyChecking=no 'ubuntu@'$HOSTNAME < mapgen.sh
 
+echo "Copying data to local disk..."
+
 scp -oStrictHostKeyChecking=no 'ubuntu@'$HOSTNAME:/tmp/tiles.tar .
-ssh -oStrictHostKeyChecking=no 'ubuntu@'$HOSTNAME shutdown -h now
+
+echo "Done. Shutting down remote host..."
+
+ssh -oStrictHostKeyChecking=no 'ubuntu@'$HOSTNAME sudo shutdown -h now
+
+echo "Map generation finished."
